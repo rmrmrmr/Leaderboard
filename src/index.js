f098ab79d9aa43fadc88d5bfeb45905e6f76db95
rmrmrmr <ricardomartineza77@gmail.com>
2022-10-26 13:39:57 -0500

    Delete Add, display and clear scores with LocSt functionality

--- src/index.js
+++ src/index.js
@@ -1,28 +1,15 @@
 import './style.css';
-import { refreshBttn, addForm, scoresWrap } from './modules/elements.js';
-import Functions from './modules/methods.js';
-
-const scoresArr = new Functions();
+import { refreshBttn, addForm } from './modules/elements.js';
+// import Functions from './modules/methods.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  scoresArr.parseLocalSt();
-  scoresArr.printHTMl();
 });
 
 addForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const nameInput = document.getElementById('nameInput');
-  const scoreInput = document.getElementById('scoreInput');
-
-  scoresArr.getValues(nameInput, scoreInput);
-  scoresArr.setIndex();
-  scoresArr.printHTMl();
-  scoresArr.addToLocalStorage();
-  Functions.clearInput(nameInput, scoreInput);
+  // const nameInput = document.getElementById('nameInput');
+  // const scoreInput = document.getElementById('scoreInput');
 });
 
 refreshBttn.addEventListener('click', () => {
-  scoresWrap.innerHTML = '';
-  localStorage.clear();
-  scoresArr.deleteArr();
 });
